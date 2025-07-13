1b7c388e677fed602d29e6cc066df109db77ae78
AlessandroRuggiero <alessandro.ruggiero.dev@gmail.com>
2025-07-13 14:59:33 +0200

    first commit

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,31 @@ mod exchange_outpost;
 use crate::exchange_outpost::FinData;
 use extism_pdk::{encoding, plugin_fn, FnResult, Json, ToBytes};
 use serde::Serialize;
+use chrono::{self, DateTime};
 
 #[derive(Serialize, ToBytes)]
 #[encoding(Json)]
-pub struct Output {}
+pub struct Output {
+    out: String
+}
 
 #[plugin_fn]
 pub fn run(fin_data: FinData) -> FnResult<Output> {
-    Ok(Output {})
+    let mut out = String::new();
+    for l in fin_data.get_labels() {
+        let candles = fin_data.get_candles(l).unwrap();
+        for candle in candles {
+            let human_readable_date = DateTime::from_timestamp(candle.timestamp / 1000, 0).unwrap().format("%Y-%m-%d %H:%M:%S").to_string();
+            out += &format!(
+                "{} {} {} {} {} {}\n",
+                l,
+                human_readable_date,
+                candle.open,
+                candle.high,
+                candle.low,
+                candle.close
+            );
+        }
+    }
+    Ok (Output { out: out.to_string() })
 }
