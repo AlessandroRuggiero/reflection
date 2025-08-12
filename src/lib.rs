59d3cce19bf9bf4ecd9cb0f23d2725056bc3d5b8
AlessandroRuggiero <alessandro.ruggiero.dev@gmail.com>
2025-08-12 15:56:28 +0200

    len

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,7 @@ use extism_pdk::{plugin_fn, FnResult};
 #[plugin_fn]
 pub fn run(fin_data: FinData) -> FnResult<String> {
     let mut out = String::new();
-    //let candles = fin_data.get_candles("symbol_1")?;
-    // for candle in candles {
-    //     out += &format!(
-    //         "{} {} {} {} {} {}\n",
-    //         candle.timestamp,
-    //         candle.open,
-    //         candle.high,
-    //         candle.low,
-    //         candle.close,
-    //         candle.volume
-    //     );
-    // }
-    out += &format!("Labels: {:?}\n", fin_data.get_labels());
+    let candles = fin_data.get_candles("symbol_1")?;
+    out += &format!("len: {}", candles.len());
     Ok(out)
 }
