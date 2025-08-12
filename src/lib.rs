804f517432918fa9698758a972543ca20a93b8c5
AlessandroRuggiero <alessandro.ruggiero.dev@gmail.com>
2025-08-12 15:43:53 +0200

    labels

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,18 @@ use extism_pdk::{plugin_fn, FnResult};
 #[plugin_fn]
 pub fn run(fin_data: FinData) -> FnResult<String> {
     let mut out = String::new();
-    let candles = fin_data.get_candles("symbol_1").unwrap();
-        for candle in candles {
-            out += &format!(
-                "{} {} {} {} {} {}\n",
-                candle.timestamp,
-                candle.open,
-                candle.high,
-                candle.low,
-                candle.close,
-                candle.volume
-            );
-        }
+    //let candles = fin_data.get_candles("symbol_1")?;
+    // for candle in candles {
+    //     out += &format!(
+    //         "{} {} {} {} {} {}\n",
+    //         candle.timestamp,
+    //         candle.open,
+    //         candle.high,
+    //         candle.low,
+    //         candle.close,
+    //         candle.volume
+    //     );
+    // }
+    out += &format!("Labels: {:?}\n", fin_data.get_labels());
     Ok(out)
 }
